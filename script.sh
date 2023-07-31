8536caba598f112aba95c39a984a6d4ee2414582
abidh123 <57485223+abidh123@users.noreply.github.com>
2023-07-31 15:31:09 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,9 +2,6 @@
           PATH_1="src/Kerry.SSP.Orders.API"
           PATH_2="src/Kerry.SSP.User.API"
           CHANGED_FILES=$(git diff HEAD HEAD~ --name-only)
-          Orders_Changed= "true"
-          User_Changed= "true"
-          
 
  
 
@@ -12,12 +9,8 @@
                         for FILE in $CHANGED_FILES; do
                           if [[ $FILE == *$PATH_1* ]]; then
                             echo "MATCH:  ${FILE} changed"
-                            echo "##vso[task.setvariable variable=Orders_Changed]true"
-                            echo "$Orders_Changed"
                           elif [[ $FILE == *$PATH_2* ]]; then
                             echo "MATCH:  ${FILE} changed"
-                            echo "##vso[task.setvariable variable=User_Changed]true"
-                            echo "$User_Changed
                           else
                             echo "IGNORE: ${FILE} changed"
                           fi
